048335a409bfcc3266ceab343c2b9c70526472a1
Mac Evans <maclean.evans@gmail.com>
2015-12-16 17:22:32 -0600

    testing again

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,7 +23,7 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
+        controller: 'MacsMainCtrl',
         controllerAs: 'main'
       })
       .when('/about', {
