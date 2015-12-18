59e5c12fda567c395d8ae10a6ddd7ef30db1a954
Daniel Sato <dksato@gmail.com>
2015-12-18 15:57:31 -0600

    Got Google places searches working (but not doing anything).

--- client/src/mapview.js
+++ client/src/mapview.js
@@ -6,18 +6,25 @@ angular.module('pegasys.mapview',['uiGmapgoogle-maps'])
         libraries: 'places,geometry'
     });
   })
-  .controller('MapviewController', function($scope,Mapview,uiGmapGoogleMapApi) {
-    uiGmapGoogleMapApi.then(function(maps) {
+  .controller('MapviewController', function($log,$scope,Mapview,uiGmapGoogleMapApi) {
+      var events = {
+        places_changed: function (searchBox) {}
+      }
+      $scope.startSearchBox = {events:events,template:'startSearchBox.tpl.html'}
+      $scope.endSearchBox = {events:events,template:'endSearchBox.tpl.html'}
+    //uiGmapGoogleMapApi.then(function(maps) {
 
       $scope.isDriver = true; //We'll ultimately pull this from the cookie
       $scope.startPointString = '' //This will go to the Google Places API
       $scope.endPointString = '' //This will go to the Google Places API
       $scope.welcome = 'Enter Your Route';
+
       $scope.map = {
         center: {latitude: 30.2932637, longitude: -97.7571322},
         zoom: 10
       }
-    });
+
+   // });
 
     /*$scope.initMap = function(){
       map = new google.maps.Map(document.getElementById('map'), {
