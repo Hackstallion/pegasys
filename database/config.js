2af8b1b2bf108d9b01d2ff9837d40613e536169d
Salman Oskooi <soskooi@gmail.com>
2015-12-18 13:54:15 -0600

    MongoDB up and running

--- database/config.js
+++ database/config.js
@@ -1,9 +1,9 @@
 
-var mongoose = require('mongoose');
-module.exports.users = Users;
+ var mongoose = require('mongoose');
 
-if (!process.env.PORT) {mongoose.connect('mongodb://localhost:27017/Users')}
-else mongoose.connect('mongodb://hackstallion:hackstallion@ds043082.mongolab.com:43082/pegasys-ride-share')
+
+// if (!process.env.PORT) {mongoose.connect('mongodb://localhost:27017')}
+mongoose.connect('mongodb://hackstallion:hackstallion@ds043082.mongolab.com:43082/pegasys-ride-share')
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -13,7 +13,7 @@ db.once('open', function (callback) {
 
 var usersSchema = mongoose.Schema({
     driver: { type: Boolean, default: false },
-    matched: { type: int, default: 0 },
+    matched: { type: Number, default: 0 },
     username: String,
     password: String,
     startPoint: Array,
@@ -29,25 +29,8 @@ var usersSchema = mongoose.Schema({
 
 var Users = mongoose.model('Users',usersSchema);
 
+// Users.collection.insert ({"sample3": "test3"});
 
-app.get('/Users', function(req, res) {
-    mongoose.model('Users').find(function(err, users) {
-        if (err) {
-            res.send(err);
-        }
-        res.send(users);
-    });
-});
-
-app.post('/Users', function(req, res) {
-    mongoose.model('Users').create({
-        driver: req.body.driver,
-
-    }, function(err, users) {
-        if (err) {
-            res.send(err);
-        }
-        res.send(users);
-    })
-})
-
+//BOTH MODEL AND CONNECTION NEED TO BE EXPORTED TO SERVER
+module.exports.users = Users;
+module.exports.db = db;
