5ded8683a5720151a2064e5893d8fc695c9ba587
Daniel Sato <dksato@gmail.com>
2015-12-21 16:08:27 -0600

    Map view page does points, routes, redraws, and submits.

--- client/src/mapview.js
+++ client/src/mapview.js
@@ -6,52 +6,111 @@ angular.module('pegasys.mapview',['uiGmapgoogle-maps'])
         libraries: 'places,geometry'
     });
   })
-  .controller('MapviewController', function($log,$scope,Mapview,uiGmapGoogleMapApi) {
-      var startPoint = [];
-      var endPoint = [];
-      var startEvents = {
-        places_changed: function (searchBox) {
-          var loc = searchBox.getPlaces()[0].geometry.location;
-          startPoint = [loc.lat(),loc.lng()];
+  .controller('MapviewController', function($log,$scope,Mapview,uiGmapGoogleMapApi,uiGmapIsReady,DB) {
+    var startPoint = [];
+    var endPoint = [];
+    var routeArray = [];
+    $scope.startMarker = null;
+    $scope.endMarker = null;
+    $scope.renderer = null;
+    $scope.welcome = 'Enter Your Route';
+    $scope.isDriver = false; //We'll ultimately pull this from the cookie
+    var startEvents = {
+      places_changed: function (searchBox) {
+        var loc = searchBox.getPlaces()[0].geometry.location;
+        startPoint = [loc.lat(),loc.lng()];
+        $scope.submitPoints();
+      }
+    }
+    var endEvents = {
+      places_changed: function (searchBox) {
+        var loc = searchBox.getPlaces()[0].geometry.location;
+        endPoint = [loc.lat(),loc.lng()];
+        $scope.submitPoints();
+      }
+    }
+    $scope.map = {
+      control: {},
+      center: {latitude: 30.268995, longitude: -97.740679}, //MakerSquare :)
+      zoom: 12
+    }
+    $scope.startSearchBox = {
+      events:startEvents,
+      template:'startSearchBox.tpl.html',
+      options: {}
+    }
+    $scope.endSearchBox = {
+      events:endEvents,
+      template:'endSearchBox.tpl.html',
+      control: {}
+    }
+
+    $scope.saveInfo = function(){
+      if ($scope.isDriver && routeArray.length){
+        DB.postRequest('route',{
+          route: routeArray,
+          bounds: {} //fill me in!
+        });
+      }
+      else if (startPoint.length && endPoint.length){
+        DB.postRequest('endpoints',{
+          startPoint: startPoint,
+          endPoint: endPoint
+        })
+      }
+      $log.log('post request submitted')
+    }
+
+    uiGmapGoogleMapApi.then(function(maps) { 
+      uiGmapIsReady.promise().then(function(instance) {
+        var map = instance[0].map;
+        $scope.submitPoints = function (){
+          if ($scope.startMarker instanceof maps.Marker && $scope.startMarker instanceof maps.Marker){
+            $scope.startMarker.setMap(null);
+            $scope.endMarker.setMap(null);
+          }
+          if ($scope.renderer instanceof maps.DirectionsRenderer) {
+            $scope.renderer.setMap(null);
+            $scope.renderer = null;
+          }
+          if (startPoint.length && endPoint.length){
+            if ($scope.isDriver){
+              var service = new maps.DirectionsService();
+              var renderer = $scope.renderer = new maps.DirectionsRenderer({
+                draggable: true,
+                map: map
+              });
+              service.route({
+                origin: new maps.LatLng(startPoint[0],startPoint[1]),
+                destination: new maps.LatLng(endPoint[0],endPoint[1]),
+                travelMode: 'DRIVING'
+                },function(result){
+                  renderer.setDirections(result);
+                  routeArray = renderer.getDirections().routes[0].overview_path.map(function(coord){
+                    return [coord.lat(),coord.lng()]
+                  });
+              });
+            }
+            else {
+              $scope.startMarker = new maps.Marker({
+                    position: new maps.LatLng(startPoint[0],startPoint[1]),
+                    map: map,
+                    title: 'Start',
+                    draggable: false 
+              });
+              $scope.endMarker = new maps.Marker({
+                    position: new maps.LatLng(endPoint[0],endPoint[1]),
+                    map: map,
+                    title: 'End',
+                    draggable: false 
+              });
+              var newBounds = new maps.LatLngBounds();
+              newBounds.extend($scope.startMarker.getPosition())
+              newBounds.extend($scope.endMarker.getPosition())
+              map.fitBounds(newBounds);
+            }
+          }
         }
-      }
-      var endEvents = {
-        places_changed: function (searchBox) {
-          var loc = searchBox.getPlaces()[0].geometry.location;
-          endPoint = [loc.lat(),loc.lng()];
-        }
-      }
-      $scope.startSearchBox = {events:startEvents,template:'startSearchBox.tpl.html'}
-      $scope.endSearchBox = {events:endEvents,template:'endSearchBox.tpl.html'}
-
-      $scope.isDriver = true; //We'll ultimately pull this from the cookie
-      $scope.welcome = 'Enter Your Route';
-
-      $scope.map = {
-        center: {latitude: 30.2932637, longitude: -97.7571322},
-        zoom: 10
-      }
-
-   // });
-
-    /*$scope.initMap = function(){
-      map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 30.2932637, lng: -97.7571322},
-      zoom: 10
-    });
-    var startPosition = new google.maps.LatLng(30.3564111,-97.7404042);
-    var endPosition = new google.maps.LatLng(30.2689996,-97.7428731);
-    var service = new google.maps.DirectionsService();
-    var renderer = new google.maps.DirectionsRenderer({
-      draggable: true,
-      map: map
-    });
-    service.route({
-      origin: startPosition,
-      destination: endPosition,
-      travelMode: 'DRIVING'
-      },function(result){
-        renderer.setDirections(result);
-    });
-    }*/
+      });
+    })
   });
