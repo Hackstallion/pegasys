54e8c900ac538a61344ff0ccb27adb60d4967fab
Mac Evans <maclean.evans@gmail.com>
2015-12-21 18:47:25 -0600

    Added model of database calls with the login page

---
+++ server/db_interface(middleware).js
@@ -0,0 +1,41 @@
+var bodyParse = require('body-parser');
+var morgan = require('morgan');
+// var app = express();
+
+
+
+model.exports = function (app, express) {
+	var loginRouter = express.Router();
+	var signUpRouter = express.Router();
+	var driverSwitchRouter = express.Router();
+	var endPointsRouter = express.Router();
+	var driverRouteRouter = express.Router();
+	var driversRouter = express.Router();
+	var profileRouter = express.Router();
+	var matchRouter = express.Router();
+	var inboxRouter = express.Router();
+	var sentRouter = express.Router();
+
+})
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../../client'));
+
+app.use('/api/users', userRouter); // use user router for all user request
+app.use('/api/links', linkRouter); // user link router for link request
+app.use('/api/login',loginRouter);
+app.use('api/signup', signUpRouter);
+app.use('api/driver', driverSwitchRouter);
+app.use('api/locations', endPointsRouter);
+app.use('api/driver-route', driverRouteRouter);
+app.use('api/show-drivers', driversRouter);
+app.use('api/profile', profileRouter);
+app.use('api/matches', matchRouter);
+app.use('api/inbox', inboxRouter);
+app.use('api/sent-messages', sentRouter);
+  // app.use(helpers.errorLogger);
+  // app.use(helpers.errorHandler);
+
+require('../login/loginRoutes.js')(loginRouter);
---
+++ server/login/loginController.js
@@ -0,0 +1,15 @@
+var Login = require('../databse/config.js');
+
+module.exports = {
+	checkUser = function (req, res, next) {
+		Users.find(function(err, user) {
+			console.log('req.body', req.body)
+			if(req.body.username === user.username && req.body.password === user.password) {
+				res.send(200)
+			}
+		})
+		next();
+	}
+}
+
+//Not worrying about tokens or hashed passwords until phase 2 
