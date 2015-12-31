6102646f2409125b858d6a673a9162d9c5c40d26
Salman Oskooi <soskooi@gmail.com>
2015-12-31 11:32:09 -0600

    consolidated createtrip routes & functions (adding/deleting trips)

--- server/locations/locationsController.js
+++ server/locations/locationsController.js
@@ -3,33 +3,7 @@ var UserModel = require('../../database/config.js'),
     Q = require('q');
 
 module.exports = {
-  newTrip: function (req, res, next) {
-    var findUser = Q.nbind(Users.findOne, Users),
-        username  = req.cookies.user,
-        tripName = Object.keys(req.body)[0];
-
-    console.log("tripName", tripName);
-    console.log("username", username);
-
-    findUser({username: username})
-      .then(function(foundUser) {
-        if (foundUser) {
-          var oldTrips = JSON.parse(foundUser.trips);
-          oldTrips[tripName] = req.body[tripName];
-          foundUser.trips = JSON.stringify(oldTrips);
-          foundUser.save();
-          res.sendStatus(200);
-        } else {
-          res.sendStatus(401);
-        }
-      })
-      .fail(function (error) {
-        next(error);
-        res.sendStatus(404);
-      });
-  },
-
-  delTrip: function (req, res, next) {
+  tripEdit: function (req, res, next) {
     var findUser = Q.nbind(Users.findOne, Users),
         username  = req.cookies.user,
         tripName = Object.keys(req.body)[0];
@@ -38,7 +12,11 @@ module.exports = {
       .then(function(foundUser) {
         if (foundUser) {
           var oldTrips = JSON.parse(foundUser.trips);
-          delete oldTrips[tripName];
+          if (req.body[tripName]["remove"]) {
+            delete oldTrips[tripName];
+          } else {
+            oldTrips[tripName] = req.body[tripName];
+          }
           foundUser.trips = JSON.stringify(oldTrips);
           foundUser.save();
           res.sendStatus(200);
@@ -52,7 +30,6 @@ module.exports = {
       });
   }
 }
-
 
 
 
--- server/locations/locationsRoutes.js
+++ server/locations/locationsRoutes.js
@@ -2,6 +2,5 @@ var locationsController = require('./locationsController.js');
 
 module.exports = function (app) {
 
-  app.post('/newtrip', locationsController.newTrip);
-  app.post('/deltrip', locationsController.delTrip);
+  app.post('', locationsController.tripEdit);
 }
