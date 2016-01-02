330c74bb618dcdbd11084bd59196301f349332b3
Daniel Sato <dksato@gmail.com>
2016-01-02 15:25:47 -0600

    Revert "Removed an unnecessary comment"
    This reverts commit fa542c097e8827e949ef3af506a8fac017bfd85b.

--- client/src/main.js
+++ client/src/main.js
@@ -45,6 +45,10 @@ angular.module('pegasys.main',[])
             })
     };
 
+    // Run init
+    
+
+
     $scope.getMatches = function(tripName,tripDriver){
       Global.setItem('currentTrip',{name: tripName, driver: tripDriver==='driver' ? true : false});
       $location.path('/match');
